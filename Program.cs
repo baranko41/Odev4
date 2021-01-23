230a9206861bbbc4016c662f3b3df4c9ff2d1fa5
Baran Köse <barankose@hotmail.com>
2021-01-23 20:48:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary_odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, byte> myDictionary = new MyDictionary<string, byte>();
+            myDictionary.Add("Ankara", 06);
+            myDictionary.Add("Elazığ", 23);
+            myDictionary.Add("Malatya", 44);
+
+            myDictionary.Prints();
+        }
+    }
+
+}
